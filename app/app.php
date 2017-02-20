fff5e7e8d02036268832459f0fbe7cc34e8aa90d
jay freeman and Sarah Leahy <sarah@>
2017-02-20 11:58:07 -0800

    create initial files and .gitignore

---
+++ app/app.php
@@ -0,0 +1,21 @@
+
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost8889;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+    return $app;
+?>
